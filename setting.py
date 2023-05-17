0aeb395d42e4649a0a73c98839400c517be609d9
SeaHyuong <seamayet@inha.edu>
2023-05-17 22:26:11 +0900

    second commit

--- setting.py
+++ setting.py
@@ -1,18 +1,18 @@
 import pygame
 pygame.init()
-player_left =   [pygame.transform.rotozoom(pygame.image.load("./image/Gunhi/Gunhi2.png"),0,0.1),
-                 pygame.transform.rotozoom(pygame.image.load("./image/Gunhi/Gunhi1.png"),0,0.1),
-                 pygame.transform.rotozoom(pygame.image.load("./image/Gunhi/Gunhi3.png"),0,0.1),
-                 pygame.transform.rotozoom(pygame.image.load("./image/Gunhi/Gunhi1.png"),0,0.1)]
+player_left =   [pygame.transform.rotozoom(pygame.image.load("./image/Gunhi/2.png"),0,0.1),
+                 pygame.transform.rotozoom(pygame.image.load("./image/Gunhi/1.png"),0,0.1),
+                 pygame.transform.rotozoom(pygame.image.load("./image/Gunhi/3.png"),0,0.1),
+                 pygame.transform.rotozoom(pygame.image.load("./image/Gunhi/1.png"),0,0.1)]
 
-player_up =     [pygame.transform.rotozoom(pygame.image.load("./image/Gunhi/Gunhi5.png"),0,0.1),
-                 pygame.transform.rotozoom(pygame.image.load("./image/Gunhi/Gunhi4.png"),0,0.1),
-                 pygame.transform.rotozoom(pygame.image.load("./image/Gunhi/Gunhi6.png"),0,0.1),
-                 pygame.transform.rotozoom(pygame.image.load("./image/Gunhi/Gunhi4.png"),0,0.1)]
+player_up =     [pygame.transform.rotozoom(pygame.image.load("./image/Gunhi/5.png"),0,0.1),
+                 pygame.transform.rotozoom(pygame.image.load("./image/Gunhi/4.png"),0,0.1),
+                 pygame.transform.rotozoom(pygame.image.load("./image/Gunhi/6.png"),0,0.1),
+                 pygame.transform.rotozoom(pygame.image.load("./image/Gunhi/4.png"),0,0.1)]
 
-player_down =   [pygame.transform.rotozoom(pygame.image.load("./image/Gunhi/Gunhi8.png"),0,0.1),
-                 pygame.transform.rotozoom(pygame.image.load("./image/Gunhi/Gunhi7.png"),0,0.1),
-                 pygame.transform.rotozoom(pygame.image.load("./image/Gunhi/Gunhi9.png"),0,0.1),
-                 pygame.transform.rotozoom(pygame.image.load("./image/Gunhi/Gunhi7.png"),0,0.1)]
+player_down =   [pygame.transform.rotozoom(pygame.image.load("./image/Gunhi/8.png"),0,0.1),
+                 pygame.transform.rotozoom(pygame.image.load("./image/Gunhi/7.png"),0,0.1),
+                 pygame.transform.rotozoom(pygame.image.load("./image/Gunhi/9.png"),0,0.1),
+                 pygame.transform.rotozoom(pygame.image.load("./image/Gunhi/7.png"),0,0.1)]
 
 pygame.quit()
